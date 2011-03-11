a89f959126d7fcd0800335223306ec9bbe12dced
hazz <get.off.my.suit@googlemail.com>
2011-03-11 22:48:02 +0000

    Cleaned up version 2.0 folder structure

--- 2.0/main.m
+++ 2.0/main.m
@@ -1,9 +1,9 @@
 //
 //  main.m
-//  Locations
+//  The Citizen
 //
-//  Created by Harry Maclean on 12/01/2011.
-//  Copyright City of London School 2011. All rights reserved.
+//  Created by Harry Maclean on 08/03/2011.
+//  Copyright 2011 City of London School. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
