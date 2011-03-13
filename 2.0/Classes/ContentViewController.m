69d484202f25794bcde0bc5d18f6737bd582756a
hazz <get.off.my.suit@googlemail.com>
2011-03-13 00:29:44 +0000

    still cleaning

---
+++ 2.0/Classes/ContentViewController.m
@@ -0,0 +1,75 @@
+//
+//  ContentViewController.m
+//  
+//
+//  Created by Harry Maclean on 05/02/2011.
+//  Copyright City of London School 2011. All rights reserved.
+//
+
+#import "ContentViewController.h"
+#import "HMTiledView.h"
+
+@implementation ContentViewController
+@synthesize pdf;
+
+
+
+/*
+// The designated initializer. Override to perform setup that is required before the view is loaded.
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+    if ((self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil])) {
+        // Custom initialization
+    }
+    return self;
+}
+*/
+
+#define PADDING 20
+// Implement loadView to create a view hierarchy programmatically, without using a nib.
+- (void)loadView {
+	currentPage = 1;
+	int numberOfPages = CGPDFDocumentGetNumberOfPages(pdf);
+
+	CGRect frame = [[UIScreen mainScreen] applicationFrame];
+	int navBarHeight = self.navigationController.navigationBar.frame.size.height;
+	frame.size.height -= navBarHeight;
+    scrollView = [[UIScrollView alloc] initWithFrame:frame];
+    scrollView.contentSize = CGSizeMake((frame.size.width+PADDING) * numberOfPages, frame.size.height);
+	scrollView.pagingEnabled = YES;
+	scrollView.backgroundColor = [UIColor grayColor];
+	self.view = scrollView;
+	
+	for (int i = 0; i < numberOfPages; i++) {
+		int width = frame.size.width;
+		HMTiledView * aView = [[HMTiledView alloc] initWithFrame:CGRectMake(i*(width+PADDING), 0.0, 320.0, frame.size.height)];
+		aView.page = CGPDFDocumentGetPage(pdf, i+1);
+		CGPDFPageRetain(aView.page);
+		[self.view addSubview:aView];
+		[aView release];
+	}
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+	CGRect frame = self.view.frame;
+	frame.size.width += PADDING;
+	self.view.frame = frame;
+}
+
+- (void)didReceiveMemoryWarning {
+	// Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+	
+	// Release any cached data, images, etc that aren't in use.
+}
+
+- (void)viewDidUnload {
+	// Release any retained subviews of the main view.
+	// e.g. self.myOutlet = nil;
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+@end
---
+++ 2.0/Classes/HMTiledView.m
@@ -0,0 +1,68 @@
+//
+//  HMTiledView.m
+//  UIView Test
+//
+//  Created by Harry Maclean on 23/02/2011.
+//  Copyright 2011 City of London School. All rights reserved.
+//
+
+#import "HMTiledView.h"
+#import <QuartzCore/QuartzCore.h>
+
+
+@implementation HMTiledView
+@synthesize page;
+
+- (id)initWithFrame:(CGRect)frame {
+    if ((self = [super initWithFrame:frame])) {
+        // Initialization code
+		self.backgroundColor = [UIColor grayColor];
+    }
+    return self;
+}
+
+
+
+- (void)drawRect:(CGRect)rect { 
+	delegate = [[HMViewDelegate alloc] init];
+	delegate.bounds = self.bounds;
+	delegate.page = page;
+	CATiledLayer * subLayer = [CATiledLayer layer];
+	subLayer.delegate = delegate;
+	int w = (int)self.bounds.size.width;
+	int h = (int)self.bounds.size.height;
+	subLayer.tileSize = CGSizeMake(w*2, h*2);
+	subLayer.levelsOfDetail = 3;
+	subLayer.levelsOfDetailBias = 3;
+	int width = self.bounds.size.width;
+	int height = self.bounds.size.height;
+	subLayer.frame = CGRectMake(0, 0, width, height);
+	subLayer.backgroundColor = [[UIColor whiteColor] CGColor];
+	
+	myContentView = [[UIView alloc] initWithFrame:subLayer.frame];
+	[myContentView.layer addSublayer:subLayer];
+	myContentView.backgroundColor = [UIColor greenColor];
+	
+	CGRect viewFrame = self.frame;
+	viewFrame.origin = CGPointZero;
+	UIScrollView * scrollView = [[UIScrollView alloc] initWithFrame:viewFrame];
+	scrollView.delegate = (id)self;
+	scrollView.contentSize = subLayer.frame.size;
+	scrollView.maximumZoomScale = 6;
+	scrollView.bouncesZoom = NO;
+	scrollView.zoomScale = 1;
+	[scrollView addSubview:myContentView];
+	[self addSubview:scrollView];
+}
+
+- (UIView *)viewForZoomingInScrollView:(UIScrollView *)scrollView {
+	return myContentView;
+	
+}
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
